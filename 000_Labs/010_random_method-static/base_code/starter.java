405ddf6fbd8ed8e6546ab14ef9be7d9d832801dc
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-09 20:43:30 +0000

    update

--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -11,6 +11,9 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		System.out.print(Math.random() ); 
-
+		int x=(int)(Math.random()*9);
+		System.out.print(x);
+		int y=(int)(Math.random()*100);
+		System.out.print(y);
 	}
 }
