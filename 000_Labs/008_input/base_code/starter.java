927e17ed92237557d1f5f8d1b7dd728e6aec5eb0
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-10 20:47:09 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -16,6 +16,15 @@ class starter {
 System.out.print("Enter your age-");
 int number=sc.nextInt();
 
-System.out.println("My name is "+text+" im "+number+" years old");
+System.out.print("What month were you born-");
+int num2=sc.nextInt();
+
+System.out.print("What day were you born-");
+int num3=sc.nextInt();
+
+System.out.print("What year were you born-");
+int num4=sc.nextInt();
+
+System.out.println("My name is "+text+", im "+number+" years old and I was born on "+num2+"/"+num3+"/"+num4+".");
 	}
 }
