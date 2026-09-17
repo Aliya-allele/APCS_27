dce30a9c211f07db3b0dc91ed40b034024648a49
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-17 20:30:35 +0000

    update

--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -10,6 +10,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter an integer:");
+		int number=sc.nextInt();
+		System.out.println("Please enter another integer:");
+		int number2=sc.nextInt();
+		System.out.println();
 	}
 }
--- 100_HW/HW4_Guessing_Game/base_code/starter.java
+++ 100_HW/HW4_Guessing_Game/base_code/starter.java
@@ -10,6 +10,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("The goal of the game is to guess a word with two hints!");
+		System.out.println();
+		System.out.println("It's a fruit!");
+		System.out.print("What is your guess? ");
+		String guess1=sc.nextLine();
+		System.out.println();
 	}
 }
