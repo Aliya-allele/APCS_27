a78be5dcc0bad7706dd95965d3d9f76decd62c6e
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-22 22:46:19 +0000

    update

--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -12,8 +12,25 @@ class starter {
 		System.out.println("Welcome to the Fortune Cookie Generator!");
 		System.out.println();
 		System.out.print("Password: ");
-		String password=sc.nextLine();
-
-		if (password)
+		int number=(int)(Math.random()*11);
+		System.out.println(number);
+		if (number==0){
+			System.out.print("Your relationships will prosper");
+		}
+		if (number==1){
+			System.out.print("You'll soon recieve a large sum of money");
+		}
+		if (number==2){
+			System.out.print("Kindness will open many doors for you");
+		}
+		if (number==3){
+			System.out.print("You'll win the lotery");
+		}
+		if (number==4){
+			System.out.print("You'll find a great opportunity soon");
+		}
+		if (number==5){
+			System.out.print("")
+		}
 	}
 }
