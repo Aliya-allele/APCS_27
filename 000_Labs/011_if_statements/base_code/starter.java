e8c90d01935f0e08f08ba39812e469bcb160e192
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-22 20:29:44 +0000

    update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,14 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int num=10;
+		int mun=15;
+		System.out.println("The first variable is: "+num); 
+		System.out.println("The second variable is: "+mun);
+		if (num!=mun){
+			System.out.println("The variables are different");
+
+		}
+
 	}
 }
