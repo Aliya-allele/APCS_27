3c114e51c6e0fb7898d517f1ffe11e892e65e00d
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-22 21:19:17 +0000

    update

--- 000_Labs/012_if_statements_more/base_code/starter.java
+++ 000_Labs/012_if_statements_more/base_code/starter.java
@@ -9,6 +9,22 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please input your first number: ");
+		int num=sc.nextInt();
+		System.out.println();
+		System.out.println("Please input your second number: ");
+		int mun=sc.nextInt();
+		System.out.println();
+
+		if (num!=mun){
+			System.out.println("Your numbers are different!");
+
+		}
+		if (num==mun){
+			System.out.println("Your numbers are the same!");
+
+		}
+
 	}
 }
--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -8,6 +8,12 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Welcome to the Fortune Cookie Generator!");
+		System.out.println();
+		System.out.print("Password: ");
+		String password=sc.nextLine();
+
+		if (password)
 	}
 }
