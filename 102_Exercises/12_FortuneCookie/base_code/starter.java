c6a4608ae066699730acb75efa6957466d74408c
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-23 01:31:25 +0000

    update

--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -15,22 +15,34 @@ class starter {
 		int number=(int)(Math.random()*11);
 		System.out.println(number);
 		if (number==0){
-			System.out.print("Your relationships will prosper");
+			System.out.println("Your relationships will prosper");
 		}
 		if (number==1){
-			System.out.print("You'll soon recieve a large sum of money");
+			System.out.println("You'll soon recieve a large sum of money");
 		}
 		if (number==2){
-			System.out.print("Kindness will open many doors for you");
+			System.out.println("Kindness will open many doors for you");
 		}
 		if (number==3){
-			System.out.print("You'll win the lotery");
+			System.out.println("You'll win the lotery");
 		}
 		if (number==4){
-			System.out.print("You'll find a great opportunity soon");
+			System.out.println("You'll find a great opportunity soon");
 		}
 		if (number==5){
-			System.out.print("")
+			System.out.println("There is still time.");
+		}
+		if (number==6){
+			System.out.println("You always have a choice");
+		}
+		if (number==7){
+			System.out.println("A solution will appear when you need it most");
+		}
+		if (number==8){
+			System.out.println("Remeber to make time for yourself");
+		}
+		if (number==9){
+			System.out.println("You don't owe anyone anything");
 		}
 	}
 }
