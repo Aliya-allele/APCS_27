efb32421ced43faf2dd49cb2086c35abb16b1373
Aliya-allele <aliyachiriboga13@gmail.com>
2026-09-24 01:59:14 +0000

    update p.2

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -10,6 +10,15 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Pick a number between 1 - 1000: ");
+		int number=sc.nextInt();
+		int answer=(int)(Math.random()*1000+1);
+		if (number==answer){
+			System.out.println("You got it!! The number was "+answer+".");
+		}
+		else{
+			System.out.println("Your number wasn't the random number. The number was "+answer+".");
+		}
 	}
 }
--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -11,10 +11,29 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Please enter an integer:");
+		System.out.print("Please enter an integer:");
 		int number=sc.nextInt();
-		System.out.println("Please enter another integer:");
+		System.out.print("Please enter another integer:");
 		int number2=sc.nextInt();
 		System.out.println();
+		if (number%3==0){
+			System.out.println(number+" is divisible by 3!");
+		}
+		if (number%4==0){
+			System.out.println(number+" is divisible by 4!");
+		}
+		if (number%5==0){
+			System.out.println(number+" is divisible by 5!");
+		}
+		System.out.println();
+		if (number2%3==0){
+			System.out.println(number2+" is divisible by 3!");
+		}
+		if (number2%4==0){
+			System.out.println(number2+" is divisible by 4!");
+		}
+		if (number2%5==0){
+			System.out.println(number2+" is divisible by 5!");
+		}
 	}
 }
